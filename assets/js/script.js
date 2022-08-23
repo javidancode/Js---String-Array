ea8911c2963843a9c49d2a318b918d99d47b66ba
javidancode <javidanig@code.edu.az>
2022-08-23 18:33:31 +0400

    Added Js - String, Array

---
+++ assets/js/script.js
@@ -0,0 +1,197 @@
+
+// string methods
+
+// let country = "Aerbaycan";
+
+// let text = "salam P130"
+
+// let result = text.substring(0,50) + " ...";
+
+// console.log(result);
+
+// let num = 66;
+
+// console.log(num.toString);
+
+// console.log(country.length);
+
+// for (let iterator of country) {
+//     console.log(iterator);
+// }
+
+// console.log(countyr[1]);
+
+// console.log(country.toLocaleUpperCase);
+
+// console.log(country.slice(4));
+
+// console.log(country.substring(0,7));
+
+// let country = "Azerbaycan";
+// let addStr = "Respublikasi";
+
+// console.log(country.substring(1,5));
+
+// console.log(country.replace("bay","salam"));
+
+// console.log(country.concat(addStr));
+
+// console.log(addStr.trim);
+
+// console.log(country.startsWith("A"));
+
+
+
+
+// if (country.startsWith("A")) {
+//     console.log("Yes");
+
+// }else{
+//     console.log("No");
+// }
+
+
+
+// console.log(country.charat(3));
+
+// console.log(country.charCodeat(0));
+
+// let result = country.split("-");
+
+// console.log(result);
+
+
+// let country = "Azerbaycan-Respublikasi-Mudafie-Nazirliyi";
+
+
+
+
+// for (let i = 0; i < country.length; i++) {
+//    console.log(countr[i]);
+
+// }
+
+
+
+
+// console.log(country.indexOf("Azerbaycan"));
+
+
+
+// function search(str) {
+//     if (country.indexOf(str) == -1) {
+//         console.log("Notfound");
+//         return;
+//     }
+
+//     console.log("Found");
+// }
+
+
+// search("test");
+
+
+// console.log(countyr.indexof("Azerbaycan",15));
+
+
+// let countryIndex = country.indexof("Azerbaycan",15);
+
+// let result = country.substring(0,countryIndex) + " ...";
+
+// console.log(countryIndex);
+
+// console.log(result);
+
+
+
+// let city = "Siyezen";
+
+// console.log(city.includes("y"));
+
+
+// console.log(Number(true));
+// console.log(Number(false));
+// console.log(Number("10"));
+// console.log(Number("  10"));
+// console.log(Number("10  "));
+// console.log(Number(" 10  "));
+// console.log(Number("10.33"));
+// console.log(Number("10,33"));
+// console.log(Number("10 33"));
+// console.log(Number("John"));
+
+
+// Number(true);
+// Number(false);
+// Number("10");
+// Number("  10");
+// Number("10  ");
+// Number(" 10  ");
+// Number("10.33");
+// Number("10,33");
+// Number("10 33");
+// Number("John");
+
+
+// let count = "asassassasaas";
+
+// console.log(typeof count);
+
+// let result = parseInt(count)
+
+// console.log(typeof result);
+
+
+
+// let users = ["Sadiq","Orxan","Cavid","Seid","Ilkin"];
+
+
+// console.log(users.toString());
+
+// console.log(users.join(" "));
+
+// users.push("Ramil");
+
+// users.pop();
+
+// users.unshift("salam");
+
+// users.shift(2);
+
+// console.log(users);
+
+
+// const myGrils = ["Fidan","Seadet"];
+
+// const myBoys = ["Azer","Togrul","Huseyn"]
+
+
+// const myChilderen = myGrils.concat(myBoys)
+
+// console.log(myChilderen);
+
+
+// users.splice(3,2,"Ayxan");
+
+
+// let users = ["Sadiq","Orxan","Cavid","Seid","Ilkin"];
+
+// console.log(users);
+
+// let result = users.splice(2,2, "Togrul");
+
+// console.log(users);
+
+// const fruits = ["Banana","Orange","Lemon","Apple","Mango",]
+// const citrus = fruits.slice(2,3);
+
+// console.log(citrus);
+// users.sort();
+// console.log(users);
+
+
+
+// const points = [40, 80, 20, 5, 2, 9];
+// console.log(points);
+// points.sort(function(a,b){return b-a});
+// console.log(points);
